91f1cbe46f591e13c8ff45503f5df414751f2e91
Tac Tacelosky <tacman@gmail.com>
2024-03-07 10:54:39 -0600

    use online_toggle controller

--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -1,4 +1,6 @@
 import { startStimulusApp } from '@symfony/stimulus-bundle';
+import Timeago from 'stimulus-timeago'
 
 const app = startStimulusApp();
-// app.register('popover', Popover);
+app.debug=false;
+app.register('timeago', Timeago);
